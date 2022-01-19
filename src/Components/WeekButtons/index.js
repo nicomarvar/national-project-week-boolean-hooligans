3b9877482e63219658aff8a916606329de7be448
scottwilson <swrs@hotmail.co.uk>
2022-01-19 10:16:35 +0000

    Started playing with data that will be coming back from the fecth request

--- src/Components/WeekButtons/index.js
+++ src/Components/WeekButtons/index.js
@@ -1,30 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-const weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const days = [1, 2, 3, 4, 5];
+// const weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const days = [1, 2, 3, 4, 5];
 
+let data = {
+  success: true,
+  message: "found all the weeks",
+  payload: [
+    { weekid: 1, weekname: "Week One", daysid: [1, 2, 3, 4] },
+    { weekid: 2, weekname: "Week Two", daysid: [5, 6, 7, 8] },
+    { weekid: 3, weekname: "Week Three", daysid: [9, 10, 11, 12] },
+    { weekid: 4, weekname: "Week Four", daysid: [13, 14, 15, 16] },
+    { weekid: 5, weekname: "Week Five", daysid: [17, 18, 19, 20] },
+    { weekid: 6, weekname: "Week Six", daysid: [21, 22, 23, 24] },
+    { weekid: 7, weekname: "Week Seven", daysid: [25, 26, 27, 28] },
+    { weekid: 8, weekname: "Week Eight", daysid: [29, 30, 31, 32] },
+  ],
+};
 function WeekButtons() {
-
-  const [isOn, setIsOn] = useState(false)
-  function dropDown(){
+  const [isOn, setIsOn] = useState(false);
+  function dropDown() {
     return setIsOn((isOn) => !isOn);
   }
 
   return (
     <ul id="weekList">
-      {weeks.map((week, index) => {
+      {data.payload.map(({ weekname, weekid, daysid }) => {
         return (
-            <li>
-                <button onClick={dropDown}> Week {week}</button>
-                <ul
-                    id={index}
-                    className={isOn ? "display-show" : "display-none"}
-                >
-                    {days.map((day) => {
-                        return <li>Day {day}</li>;
-                    })} 
-                </ul>
-            </li>
+          <li>
+            <button onClick={dropDown}> {weekname}</button>
+            <ul id={weekid} className={isOn ? "display-show" : "display-none"}>
+              {daysid.map((day) => {
+                return <li>Day {day}</li>;
+              })}
+               
+            </ul>
+          </li>
         );
       })}
     </ul>
