fbb01612a2956ba66faeba9da479468e9fca7021
Hyosssssun <hyosssssun@gmail.com>
2022-01-19 09:47:28 +0000

    added toggle function for side bar

--- src/Components/WeekButtons/index.js
+++ src/Components/WeekButtons/index.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 const weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const days = [1, 2, 3, 4, 5];
 
 function WeekButtons() {
+
+  const [isOn, setIsOn] = useState(false)
+  function dropDown(){
+    return setIsOn((isOn) => !isOn);
+  }
+
   return (
     <ul id="weekList">
       {weeks.map((week) => {
         return (
-          <li>
-            Week {week}
-            <ul id="dayList">
-              {days.map((day) => {
-                return <li>Day {day}</li>;
-              })}
-            </ul>
-          </li>
+            <li>
+                <button onClick={dropDown}> Week {week}</button>
+                <ul
+                    id={week}
+                    className={isOn ? "display-show" : "display-none"}
+                >
+                    {days.map((day) => {
+                        return <li>Day {day}</li>;
+                    })}
+                </ul>
+            </li>
         );
       })}
     </ul>
