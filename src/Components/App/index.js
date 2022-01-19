72cf1459f6c0e45b12ef855c87b650a38883e1a6
scottwilson <swrs@hotmail.co.uk>
2022-01-19 13:50:51 +0000

    fetch request working

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -3,6 +3,7 @@ import Sidebar from "../Sidebar";
 import Daybox from "../Daybox";
 import Topic from "../Topic";
 import Topbar from "../Topbar";
+import React, { useState, useEffect } from "react";
 
 let day = "Day 2";
 let week = "Week 3";
@@ -28,6 +29,18 @@ let resourceLinks = [
 ];
 
 function App() {
+  const [apiData, setData] = useState([9]);
+  useEffect(() => {
+    FetchApi();
+  });
+  async function FetchApi() {
+    const response = await fetch("https://boolean-hooligans.herokuapp.com/");
+    const data = await response.json();
+    console.log("called");
+    setData(data);
+  }
+  FetchApi();
+  console.log(apiData);
   return (
     <div className="App">
       <Sidebar className="sidebar" />
