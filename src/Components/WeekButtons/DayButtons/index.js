11e81840b5556b8451ea3b65a2daef8632c72c97
Ayomide Ojelade <ojeladea@outlook.com>
2022-01-20 11:10:08 +0000

    side bar css

--- src/Components/WeekButtons/DayButtons/index.js
+++ src/Components/WeekButtons/DayButtons/index.js
@@ -1,31 +1,36 @@
 import React from "react";
 
 function DayButtons({ daysid, gettingDay }) {
-    return (
-        <>
-            {daysid.map((day) => {
-                if (day % 4 === 0) {
-                    return (
-                        <li
-                            id={day}
-                            onClick={() => {gettingDay({ day })}}
-                        >
-                            Day 4
-                        </li>
-                    );
-                } else {
-                    return (
-                        <li
-                            id={day}
-                            onClick={() => {gettingDay({ day })}}
-                        >
-                            Day {day % 4}
-                        </li>
-                    );
-                }
-            })}
-        </>
-    );
+  return (
+    <>
+      {daysid.map((day) => {
+        if (day % 4 === 0) {
+          return (
+            <li
+              id={day}
+              onClick={() => {
+                gettingDay({ day });
+              }}
+            >
+              Day 4
+            </li>
+          );
+        } else {
+          return (
+            <li
+              className="dayListButton"
+              id={day}
+              onClick={() => {
+                gettingDay({ day });
+              }}
+            >
+              Day {day % 4}
+            </li>
+          );
+        }
+      })}
+    </>
+  );
 }
 
 export default DayButtons;
