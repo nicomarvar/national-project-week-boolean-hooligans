a41b5e26d9556f635558edf6622033670b6d74af
nicomarvar <nicomarvar@gmail.com>
2022-01-20 15:45:30 +0000

    Topic display

--- src/Components/Topic/index.js
+++ src/Components/Topic/index.js
@@ -1,7 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Topic({ topicId }) {
   const [links, setLinks] = useState([]);
+  const [topics, setTopic] = useState("");
+
+  useEffect(function(){
+      async function fetchTopic(){
+          const request = await fetch(`https://boolean-hooligans.herokuapp.com/subjects/${topicId[0]}`);
+          const response = await request.json();
+          console.log(response)
+          setTopic(response.payload[0].subjectname);          
+      }
+      fetchTopic();
+  }, [topicId])
 
   async function handleSubmit(e, topicId) {
     e.preventDefault();
@@ -23,28 +34,53 @@ function Topic({ topicId }) {
     setLinks(response.payload[0].links);
   }
 
-  return (
-    <div className="topics">
-      <h2 className="topicTitle">hello</h2>
-      <div className="scroll">
-        <ul>
-          {links.map((link) => {
-            return <li>{link}</li>;
-          })}
-        </ul>
+
+  return  topicId.map((topic, index)=>{
+      return <div className="topics">
+          <h2 className="topicTitle">{topics}</h2>
+          <div className="scroll">
+              <ul>
+                  {
+                    links.map((link) => {
+                        return <li>{link}</li>
+                    })
+                  }
+              </ul>
+          </div>
+               <form className="submitBox" onSubmit={(e) => { handleSubmit(e, index)}}>
+          <input className="inputBox"></input>
+          <button className="submitButton" type="submit">
+            Submit
+          </button>
+        </form>
       </div>
-      <form
-        className="submitBox"
-        onSubmit={(e) => {
-          handleSubmit(e, topicId);
-        }}
-      >
-        <input className="inputBox"></input>
-        <button className="submitButton" type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-}
+  })
+  }
+//   return (
+//     <div className="topics">
+//       {
+//           topicId.map((topic) => {
+//             return <h2 className="topicTitle">{topics}</h2>
+//           })
+//       }
+//       <div className="scroll">
+//         <ul>
+//           {links.map((link) => {
+//             return <li>{link}</li>;
+//           })}
+//         </ul>
+//       </div>
+//       <form
+//         className="submitBox"
+//         onSubmit={(e) => {
+//           handleSubmit(e, topicId);
+//         }}
+//       >
+//         <input className="inputBox"></input>
+//         <button className="submitButton" type="submit">
+//           Submit
+//         </button>
+//       </form>
+//     </div>
+//   );
 export default Topic;
