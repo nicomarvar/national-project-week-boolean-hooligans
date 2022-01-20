3c1195776088256798c6f94ad5b5715bcd777d67
scottwilson <swrs@hotmail.co.uk>
2022-01-20 15:07:09 +0000

    We now have a topicId added to topic

--- src/Components/Topic/index.js
+++ src/Components/Topic/index.js
@@ -1,27 +1,28 @@
-import React, {useState}  from "react";
+import React, { useState } from "react";
 
-function Topic({ lessonTopic, resourceLinks }) {
+function Topic({ lessonTopic, resourceLinks, topicId }) {
+  const [links, setLinks] = useState([]);
 
-    const [links, setLinks] = useState([])
+  async function handleSubmit(e, topicId) {
+    e.preventDefault();
+    let resource = e.target.children[0].value;
+    const request = await fetch(
+      `https://boolean-hooligans.herokuapp.com/subjects/${topicId}`,
+      {
+        method: "PATCH",
+        body: JSON.stringify({
+          resources: resource,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+    const response = await request.json();
+    console.log(response);
+    setLinks(response.payload[0].links);
+  }
 
-    async function handleSubmit(e, id){
-        e.preventDefault()
-        let resource = e.target.children[0].value
-        const request = await fetch(`https://boolean-hooligans.herokuapp.com/subjects/${id}`, 
-        {
-            method:"PATCH",
-            body: JSON.stringify({
-            resources: resource
-            }),
-            headers: {
-            "Content-type": "application/json; charset=UTF-8"
-            }
-        });
-        const response = await request.json();
-        console.log(response)
-        setLinks(response.payload[0].links)
-    }
-    
   return (
     <div className="topics">
       <h2 className="topicTitle">{lessonTopic}</h2>
@@ -32,9 +33,16 @@ function Topic({ lessonTopic, resourceLinks }) {
           })}
         </ul>
       </div>
-      <form className="submitBox" onSubmit={(e) => {handleSubmit(e,1)}}>
+      <form
+        className="submitBox"
+        onSubmit={(e) => {
+          handleSubmit(e, 1);
+        }}
+      >
         <input className="inputBox"></input>
-        <button className="submitButton" type="submit">Submit</button>
+        <button className="submitButton" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
